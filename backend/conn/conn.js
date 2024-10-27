27c9f9863417301a8db5589d663a6b8e0fd98f2f
Pratik Singh <pratik00888@gmail.com>
2024-10-27 10:07:31 +0530

    auth server

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const conn = async (req,res) => {
-    await mongoose.connect("mongodb+srv://pratik99672:5utxYrSIYlK2kDZ7@cluster0.pyqau.mongodb.net/").then(() => {
+    await mongoose.connect("mongodb+srv://pratik99672:5utxYrSIYlK2kDZ7@cluster0.pyqau.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
         console.log("Connected to MongoDB")
     })
 }
---
+++ backend/routes/auth.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const { User } = require("../models/user");
+
+router.post("/register", async (req,res) => {
+    try {
+        
+    }catch(error){
+        console.log(error);
+    }
+})
+
+
+
+module.exports = router;
