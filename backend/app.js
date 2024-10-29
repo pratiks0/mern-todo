316979c52863420edf5bf6b00f27863c9de7ac12
Pratik Singh <pratik00888@gmail.com>
2024-10-29 22:44:08 +0530

    signup/login

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
-require("./conn/conn")
+require("./conn/conn");
+const auth = require("./routes/auth");
+app.use(express.json());
 
-app.get("/", (req,res) => {
-    res.send("hello");
-})
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.use("/api/v1", auth);
 
 app.listen(1000, () => {
-    console.log("Server is running on port 1000");
-})
+  console.log("Server is running on port 1000");
+});
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,47 @@
 const router = require("express").Router();
-const { User } = require("../models/user");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
-router.post("/register", async (req,res) => {
+
+//sign up
+router.post("/register", async (req, res) => {
     try {
-        
-    }catch(error){
-        console.log(error);
+        const { email, username, password } = req.body;
+        const hashedPassword = bcrypt.hashSync(password);
+        const user = new User({ email, username, password: hashedPassword });
+        await user.save();
+        res.status(200).json({ user });
+    } catch (error) {
+        if (error.code === 11000) {
+            // Duplicate key error code for MongoDB
+            res.status(400).json({ message: "User Already Exists" });
+        } else {
+            res.status(500).json({ message: "Server Error", error: error.message });
+        }
+    }
+});
+
+
+//sign in 
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if(!user) {
+            return res.status(400).json({ message: "Invalid Email or Password" });
+        }
+
+        const isPasswordCorrect = bcrypt.compareSync(
+            req.body.password,
+            user.password
+        );
+        if(!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid Password" });
+        }
+        const { password, ...others } = user._doc;
+        res.status(200).json({ others })
+    } catch (error) {
+        return res.status(400).json({ message: "user already exists" });
     }
 })
-
-
 
 module.exports = router;
