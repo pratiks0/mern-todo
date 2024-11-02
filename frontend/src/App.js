2e10978af87c9f6c1f9a657e7375b6a761a4f6a3
Pratik Singh <pratik00888@gmail.com>
2024-11-02 12:45:19 +0530

    new commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import Home from './components/home/Home.jsx';
 import Footer from './components/footer/Footer.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './components/about/About.jsx';
+import Signup from './components/signup/Signup.jsx'
+import SignIn from './components/signup/SignIn.jsx'
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<SignIn />} />
         </Routes>
         <Footer />
       </BrowserRouter>
